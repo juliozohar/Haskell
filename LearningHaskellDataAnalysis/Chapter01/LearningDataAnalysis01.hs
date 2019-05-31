3317f913476be016c202988f246d379a9ddec2ff
Julio Cesar Torres <juliozohar@gmail.com>
2019-05-30 21:37:32 -0400

    Update LearningDataAnalysis01.hs

--- LearningHaskellDataAnalysis/Chapter01/LearningDataAnalysis01.hs
+++ LearningHaskellDataAnalysis/Chapter01/LearningDataAnalysis01.hs
@@ -1,1 +1,20 @@
--- Programa para calculo de mediana
+module LearningDataAnalysis01  where 
+
+-- Program to compute the median of a dataset, that is the average of the two closest 
+-- elements to the middle of the sorted dataset. 
+
+import Data.List 
+
+
+median :: [Double] -> Double
+median [] = 0
+median xs = if oddLength then 
+                middleValue 
+            else 
+                (middleValue + beforeMiddleValue) / 2
+    where 
+        sortedList = sort xs
+        oddLength = 1 == mod (genericLength xs) 2
+        middle = floor $ (genericLength xs) / 2
+        middleValue = genericIndex sortedList middle
+        beforeMiddleValue = genericIndex sortedList (middle -1)
